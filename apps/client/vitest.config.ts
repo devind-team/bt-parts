5c986aaa08e3445d34d5078ba8cf60dcb621cfe0
Luferov Victor <luferovvs@yandex.ru>
2024-06-18 10:35:39 +0300

    init: Initialize

---
+++ apps/client/vitest.config.ts
@@ -0,0 +1,32 @@
+/// <reference types='vitest' />
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+
+export default defineConfig({
+  root: __dirname,
+  cacheDir: '../../node_modules/.vite/apps/client',
+
+  plugins: [vue(), nxViteTsPaths()],
+
+  // Uncomment this if you are using workers.
+  // worker: {
+  //  plugins: [ nxViteTsPaths() ],
+  // },
+
+  test: {
+    watch: false,
+    globals: true,
+    cache: {
+      dir: '../../node_modules/.vitest/apps/client',
+    },
+    environment: 'jsdom',
+    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+
+    reporters: ['default'],
+    coverage: {
+      reportsDirectory: '../../coverage/apps/client',
+      provider: 'v8',
+    },
+  },
+});
