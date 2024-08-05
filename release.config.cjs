48049f81d2d6be15e79e931b6bc57fea7968e074
Luferov Victor <luferovvs@yandex.ru>
2024-08-05 23:12:54 +0300

    fix: Add github plugin

--- release.config.cjs
+++ release.config.cjs
@@ -2,13 +2,13 @@
  * @type {import('semantic-release').GlobalConfig}
  */
 module.exports = {
-  branches: ['master'],
+  branches: ['main'],
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
     '@semantic-release/npm',
     '@semantic-release/git',
-    '@semantic-release/github'
-  ]
+    '@semantic-release/github',
+  ],
 }
