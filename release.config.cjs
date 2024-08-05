d295b33d4b2e9dd7600e1de5457bfdfde759f266
Luferov Victor <luferovvs@yandex.ru>
2024-08-06 00:30:00 +0300

    fix: Commit release message

--- release.config.cjs
+++ release.config.cjs
@@ -8,7 +8,12 @@ module.exports = {
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
     '@semantic-release/npm',
-    '@semantic-release/git',
     '@semantic-release/github',
+    [
+      '@semantic-release/git',
+      {
+        message: 'chore(release): ${nextRelease.version}\n\n${nextRelease.notes}',
+      },
+    ],
   ],
 }
