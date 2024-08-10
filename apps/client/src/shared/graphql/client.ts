701c58c7ccd429a9d0e5854c3c24b20ebe3e2cfc
Luferov Victor <luferovvs@yandex.ru>
2024-08-10 09:37:00 +0300

    fix: Typing nuxt api url browser

--- apps/client/src/shared/graphql/client.ts
+++ apps/client/src/shared/graphql/client.ts
@@ -2,7 +2,7 @@ import { defineApolloClient } from '@nuxtjs/apollo/config'
 
 export default defineApolloClient({
   httpEndpoint: process.env.NUXT_API_URL as string,
-  browserHttpEndpoint: process.env.NUX_API_URL_BROWSER,
+  browserHttpEndpoint: process.env.NUXT_API_URL_BROWSER,
   wsEndpoint: process.env.NUXT_WS_URL_BROWSER,
   inMemoryCacheOptions: {},
   // inMemoryCacheOptions: { fragmentMatcher }
