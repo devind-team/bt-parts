3602ca5db7883e4580068182fa97617a8a2d1103
Luferov Victor <luferovvs@yandex.ru>
2024-08-14 11:38:24 +0300

    feat: Добавлена страница поиска

---
+++ apps/client/src/pages/search.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import type { BreadCrumbsItem } from '@/shared/layout/types'
+import { PrimeIcons } from '@primevue/core/api'
+import BreadCrumbs from '@/shared/layout/ui/BreadCrumbs.vue'
+const { t } = useI18n()
+
+const links = ref<BreadCrumbsItem[]>([
+  { label: t('search.title'), icon: PrimeIcons.SEARCH, name: 'search' }
+])
+
+</script>
+<template>
+  <bread-crumbs :items="links">
+    <div>Поиск</div>
+  </bread-crumbs>
+</template>
