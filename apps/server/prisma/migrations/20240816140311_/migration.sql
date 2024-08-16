de01a3c1ca38f1fbc0c200df47c1f89e1702c099
darknese <darkflame6277@gmail.com>
2024-08-16 17:03:39 +0300

    feat: unavaible and cancel status for item

---
+++ apps/server/prisma/migrations/20240816140311_/migration.sql
@@ -0,0 +1,24 @@
+-- AlterEnum
+-- This migration adds more than one value to an enum.
+-- With PostgreSQL versions 11 and earlier, this is not possible
+-- in a single migration. This can be worked around by creating
+-- multiple migrations, each migration adding only one value to
+-- the enum.
+
+
+ALTER TYPE "ItemStatus" ADD VALUE 'UNABAIBLE';
+ALTER TYPE "ItemStatus" ADD VALUE 'CANCEL';
+
+-- AlterEnum
+-- This migration adds more than one value to an enum.
+-- With PostgreSQL versions 11 and earlier, this is not possible
+-- in a single migration. This can be worked around by creating
+-- multiple migrations, each migration adding only one value to
+-- the enum.
+
+
+ALTER TYPE "OrderStatus" ADD VALUE 'REQUEST';
+ALTER TYPE "OrderStatus" ADD VALUE 'CANCEL';
+
+-- AlterTable
+ALTER TABLE "products" ADD COLUMN     "vin_number" VARCHAR(128);
